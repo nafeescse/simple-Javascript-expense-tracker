72d0cfc030389b2896106c9ef030ba888c5abcdd
Nafees <abdur17@student.sust.edu>
2022-02-17 23:34:50 +0600

    almost  done

--- js/apps.js
+++ js/apps.js
@@ -9,45 +9,60 @@ const save = document.getElementById('save-input');
 const saveButton = document.getElementById('save-button');
 const savingAmount = document.getElementById('saving-amount');
 const remainingBalance = document.getElementById('remaining-balance');
+// Function for getting input fields
+function getInput(input) {
+    if (input.value >= 0) {
+        const Cost = input.value;
+        return Cost;
+    }
+    else if (input.value < 0) {
+        window.alert('Enter a valid amount!!');
+        return 0;
 
-function getInput(input){
-        if(input.value >=  0){
-            const Cost =  input.value;
-            return Cost;
-        }
-        else if(input.value <  0 ){
- 
-            window.alert('Enter a valid amount!!');
-        }else if(typeof(input.value) == 'string'){
-            window.alert('Enter a valid amount not string!!');
-        }
-    
+    } else if (typeof (input.value) == 'string') {
+        window.alert('Enter a valid amount not string!!');
+        return 0;
+    }
 }
 // debugger;
+// Event handler for calculate button
+calcButton.addEventListener('click', function () {
 
-saveButton.addEventListener('click', function(){
-    const incomeTotal = Number(getInput(income));
-    const saveInput = Number(getInput(save));
-    savingAmount.innerText = (incomeTotal * saveInput) / 100;
-   
-    console.log(savingAmount.innerText);
-
-})
-calcButton.addEventListener('click', function(){
-    // console.log(income.value, save.value, balance.innerText);
+// Getting value by calling getInput() function
     const foodCost = Number(getInput(food));
     const rentCost = Number(getInput(rent));
     const clothCost = Number(getInput(cloth));
     const total = foodCost + rentCost + clothCost;
-    let totalExpences = expences.innerText;
-    expences.innerText = total;
     const incomeTotal = Number(getInput(income));
-    balance.innerText = incomeTotal - total;
-    if(savingAmount.innerText > balance.innerText){
-        window.alert('Savings cant be bigger than balance');
-    }else{
-    remainingBalance.innerText = balance.innerText - savingAmount.innerText;
+// Error checking as income must be greater than expences
+    if (incomeTotal < total) {
+        swal({
+            title: "Oppps!",
+            text: "Expences can't be bigger than income!!",
+            icon: "warning",
+            button: "Try Again!",
+        });
+    } else {
+        expences.innerText = total;
+        balance.innerText = incomeTotal - total;
     }
-    console.log('total', remainingBalance.innerText);
 });
+// Event handler for save button
+saveButton.addEventListener('click', function () {
+    const incomeTotal = Number(getInput(income));
+    const saveInput = Number(getInput(save));
+    savingAmount.innerText = (incomeTotal * saveInput) / 100;
+// Error checking as balance must be greater than Savings
+    if ((balance.innerText - savingAmount.innerText) < 0) {
+        swal({
+            title: "Oppps!",
+            text: "Savings can't be bigger than balance!!",
+            icon: "warning",
+            button: "Okay!",
+        });
+    } else {
+        remainingBalance.innerText = balance.innerText - savingAmount.innerText;
+    }
+})
+
 
